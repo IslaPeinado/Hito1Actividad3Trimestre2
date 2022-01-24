16fd4093743ffaf1a8baad0524265bf737fcbd89
IslaPeinado <93380467+IslaPeinado@users.noreply.github.com>
2022-01-24 14:00:24 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+    // valor decimal
+    $decimal = 153;
+    echo "<p>El numero a calcular es ".$decimal."</p>";
+
+    // Convertir a binario
+    $binario = decbin($decimal);
+   
+    // Resultado
+    echo "Numero en binario es " . $binario;
+    ?>
+
+</body>
+</html>
+
+
+
+
